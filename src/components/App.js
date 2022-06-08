bc4896297f6b671782d0be189c1513417426fad2
Joe Rupp <joerpp8@gmail.com>
2022-06-08 15:48:27 -0600

    Add initial component setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import React from "react";
+import { Route } from "react-router-dom";
+import Nav from "./Nav";
+import Home from "./Home";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <main>
+      <Nav />
+      <Route exact path="/" render={() => <Home />} />
+    </main>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import "./Home.css";
+
+function Home() {
+  return <section></section>;
+}
+
+export default Home;
