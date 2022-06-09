aa5e80507a5ec65c4d3205e00f1cf04343edf227
Joe Rupp <joerpp8@gmail.com>
2022-06-09 10:24:50 -0600

    Add your rentals page setup

--- src/components/YourRentals.js
+++ src/components/YourRentals.js
@@ -1,7 +1,13 @@
 import React from "react";
+import "./YourRentals.css";
 
 function YourRentals() {
-  return <section></section>;
+  return (
+    <section className="yourRentalsSections">
+      <p>All your current rentals can be found below:</p>
+      <div className="yourRentals"></div>
+    </section>
+  );
 }
 
 export default YourRentals;
