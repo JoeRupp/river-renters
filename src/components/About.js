bd52eb0dd601265feaab86184712e2aedd79a6cc
Joe Rupp <joerpp8@gmail.com>
2022-06-11 09:08:07 -0600

    Update favicon and about page

--- src/components/About.js
+++ src/components/About.js
@@ -3,12 +3,26 @@ import "./About.css";
 
 function About() {
   return (
-    <section>
-      <img
-        className="homeImage"
-        src={require("../assets/photos/Rafting-5.png")}
-        alt="Three people rafting in a blue boat with the trees and mountains behind them"
-      />
+    <section className="wrapper">
+      <div classname="section parallax bg1">
+        <img
+          className="aboutImage"
+          src={require("../assets/photos/Rafting-5.png")}
+          alt="Three people rafting in a blue boat with the trees and mountains behind them"
+        />
+        <h1 className="catchPhrase">WE HELP YOU GET OUT THERE</h1>
+      </div>
+      <div classname="section static">
+        <p>
+          We help you get out there and experience the world. Our rafts start
+          with the demands of the river culture, and we use technical materials,
+          carefully crafted to create a modern, stylish look. Make no mistake,
+          we get out there - whatever the weather.
+        </p>
+      </div>
+      <div classname="section parallax bg2">
+        <h1>SO GET OUT THERE!</h1>
+      </div>
     </section>
   );
 }
