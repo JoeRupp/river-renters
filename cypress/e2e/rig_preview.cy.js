9dd94bb3d1c915a56d6d01cb275ee98f1528a1f0
Joe Rupp <joerpp8@gmail.com>
2022-06-12 14:44:35 -0600

    Add rig preview and your rental tests

--- cypress/e2e/rig_preview.cy.js
+++ cypress/e2e/rig_preview.cy.js
@@ -3,7 +3,59 @@ describe("River Renters rig preview", () => {
     cy.intercept("GET", "https://river-renters-api.herokuapp.com/api/v1/rigs", {
       fixture: "./raftData.json",
     }).visit("http://localhost:3000/");
+    cy.intercept(
+      "PATCH",
+      "https://river-renters-api.herokuapp.com/api/v1/rigs/1"
+    );
+    cy.intercept(
+      "PATCH",
+      "https://river-renters-api.herokuapp.com/api/v1/rigs/6"
+    );
   });
 
-  it("should display the home page with the navigation and rent a rig display", () => {});
+  it("should be able to navigate there from the rent a rig screen by selecting a rig card", () => {
+    cy.get(".rigCard").first().click();
+    cy.url().should("include", "/raft/1");
+  });
+
+  it("should be able to navigate there from your rentals screen by selecting a rig card", () => {
+    cy.visit("http://localhost:3000/your-rentals");
+    cy.get(".rigCard").first().click();
+    cy.url().should("include", "/raft/6");
+  });
+
+  it("should display all boat information", () => {
+    cy.get(".rigCard").first().click();
+    cy.contains("NRS Expedition-136 - fully rigged");
+    cy.contains("NRS | Multi-day");
+    cy.contains(
+      "The 13 ft 6in NRS E-136 plays like a 13' boat and works like a 14' boat. Its aggressive rocker and wide stance make it fun but dependable in the rapids. Great for fishing, overnights and just seeking thrills."
+    );
+    cy.contains("$195 per day");
+    cy.get("img")
+      .last()
+      .should("have.attr", "src")
+      .should("include", "/static/media/rigs-NRS-4.0977c83642013e888171.png");
+  });
+
+  it("should have a button allowing the user to rent the boat and guiding the user to the You Rentals view", () => {
+    cy.get(".rigCard").first().click();
+    cy.get("button").last().click();
+    cy.url().should("include", "/your-rentals");
+    cy.get(".rigCard")
+      .should("have.length", 2)
+      .first()
+      .should(
+        "have.text",
+        "NRS Expedition-136 - fully riggedNRS | Multi-day$195 per day"
+      );
+  });
+
+  it("should have a button allowing the user to return the boat and guiding the user to the You Rentals view", () => {
+    cy.visit("http://localhost:3000/your-rentals");
+    cy.get(".rigCard").first().click();
+    cy.get("button").last().click();
+    cy.url().should("include", "/your-rentals");
+    cy.get(".rigCard").should("have.length", 0);
+  });
 });
--- cypress/e2e/your_rentals.cy.js
+++ cypress/e2e/your_rentals.cy.js
@@ -3,7 +3,61 @@ describe("River Renters your rentals display", () => {
     cy.intercept("GET", "https://river-renters-api.herokuapp.com/api/v1/rigs", {
       fixture: "./raftData.json",
     }).visit("http://localhost:3000/");
+    cy.intercept(
+      "PATCH",
+      "https://river-renters-api.herokuapp.com/api/v1/rigs/6"
+    );
   });
 
-  it("should display the home page with the navigation and rent a rig display", () => {});
+  it("should be able to navigate to your rentals from the navigation bar", () => {
+    cy.get(".btn").first().click();
+    cy.url().should("include", "/your-rentals");
+    cy.contains("It looks like you don't have any rentals right now!").should(
+      "not.exist"
+    );
+  });
+
+  it("should be able to navigate to your rentals with a url", () => {
+    cy.visit("http://localhost:3000/your-rentals");
+    cy.contains("All your current rentals can be found below:");
+  });
+
+  it("should display all current rentals", () => {
+    cy.visit("http://localhost:3000/your-rentals");
+    cy.get(".yourRentals").should("exist");
+    cy.get(".rigCard")
+      .should("have.length", 1)
+      .last()
+      .should(
+        "have.text",
+        "Maravia 14X24 Cataraft - fully riggedMaravia | Multi-day$215 per day"
+      )
+      .get("img")
+      .last()
+      .should("have.attr", "src")
+      .should(
+        "include",
+        "/static/media/rigs-Maravia-6.ba9f7b8d37ad33426ea1.png"
+      );
+  });
+
+  it("should show a message saying the user has no rentals if none are populated.", () => {
+    cy.visit("http://localhost:3000/your-rentals");
+    cy.get(".rigCard").first().click();
+    cy.get("button").last().click();
+    cy.url().should("include", "/your-rentals");
+    cy.get(".rigCard").should("have.length", 0);
+    cy.contains("It looks like you don't have any rentals right now!");
+  });
+
+  it("should go a 404 error page if the wrong url is entered", () => {
+    cy.visit("http://localhost:3000/kjdjkdsjkd");
+    cy.get("img")
+      .last()
+      .should("have.attr", "src")
+      .should(
+        "include",
+        "/static/media/Rafting404Error.ce933bcc9b987f5f0aea.png"
+      );
+  });
 });
