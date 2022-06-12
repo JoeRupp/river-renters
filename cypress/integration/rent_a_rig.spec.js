8c07ea2873b8022b01c5800d7307709000be86f0
Joe Rupp <joerpp8@gmail.com>
2022-06-12 13:45:59 -0600

    Add rent a rig tests

--- cypress/integration/rent_a_rig.spec.js
+++
@@ -1,9 +0,0 @@
-describe("River Renters rent a rig display", () => {
-  beforeEach(() => {
-    cy.intercept("GET", "https://river-renters-api.herokuapp.com/api/v1/rigs", {
-      fixture: "./raftData.json",
-    }).visit("http://localhost:3000/");
-  });
-
-  it("should display the home page with the navigation and rent a rig display", () => {});
-});
